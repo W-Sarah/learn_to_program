00cd4a0a1d1b136ef91dcd97a385ed49a1b9745e
W-Sarah <sarahweiss75@gmail.com>
2014-04-11 16:43:02 +0900

    adding chapter 5 exercise file

---
+++ chapter_5.rb
@@ -0,0 +1,8 @@
+#Full name greeting
+puts "Hi, What's your first name?"
+first_name = gets.chomp
+puts "What's your middle name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Welcome, #{first_name} #{middle_name} #{last_name}!"
