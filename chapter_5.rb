8f31d0f7f95aa6efc9f896b81a9bd4ca0770c992
W-Sarah <sarahweiss75@gmail.com>
2014-04-11 16:47:12 +0900

    chapter 5 exercise completed

--- chapter_5.rb
+++ chapter_5.rb
@@ -6,3 +6,10 @@ middle_name = gets.chomp
 puts "What's your last name?"
 last_name = gets.chomp
 puts "Welcome, #{first_name} #{middle_name} #{last_name}!"
+
+
+#Bigger, better favorite number
+puts "Hi What's your favorite number?"
+favorite_number = gets.chomp.to_i
+bigger_number = favorite_number + 1
+puts "#{bigger_number} is a bigger and better favorite number!"
